6b7e84a6a3ec0fed00c6162e00398e9ae36cdaed
Jesper Christensen <jesper300595@gmail.com>
2019-05-01 00:23:06 +0200

    simple googlemaps implemented

--- src/pages/Map.js
+++ src/pages/Map.js
@@ -1,9 +1,29 @@
 import React, { Component } from 'react';
+import { Map, GoogleApiWrapper } from 'google-maps-react';
 
-class Map extends Component {
-    render() { 
-        return ( <h1>Map Test</h1> );
-    }
+const mapStyles = {
+    width: '70%',
+    height: '50%',
+    display: 'flex',
+    borderColor: 'black',
+};  
+
+export class MapContainer extends Component {
+  render() {
+    return (
+      <Map
+        google={this.props.google}
+        zoom={14}
+        style={mapStyles}
+        initialCenter={{
+         lat: -1.2884,
+         lng: 36.8233
+        }}
+      />
+    );
+  }
 }
- 
-export default Map;
+
+export default GoogleApiWrapper({
+  apiKey: 'API-KEY-HERE'
+})(MapContainer);
